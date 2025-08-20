9d5573c451388563e09792c39997bbccba7afc69
Wescley <wescleygaldino06@gmail.com>
2025-08-20 16:29:36 -0300

    Adicionando função de pesquisar (básico) User Story #10

--- repex/urls.py
+++ repex/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('explorar/', views.explorar, name='explorar'),
+    path("pesquisar/", views.pesquisar, name = "pesquisar"),
 ]
--- repex/views.py
+++ repex/views.py
@@ -2,10 +2,41 @@ from django.shortcuts import render
 from repex.models import Projeto, Noticia
 
 def index(request):
-    projetos = Projeto.objects.all()
+    projetos = Projeto.objects.all().order_by("criado_em")
     noticias = Noticia.objects.all()
     context = {
         "projetos": projetos,
         "noticias": noticias
     }
     return render(request, 'index.html', context)
+
+def explorar(request):
+    projetos = Projeto.objects.all()
+    noticias = Noticia.objects.all()
+    context = {
+        "projetos": projetos,
+        "noticias": noticias
+    }
+    return render(request, 'explorar.html', context)
+
+def pesquisar(request):
+    query = request.GET.get('q', '')
+    
+    resultados_titulo = Projeto.objects.filter(titulo__icontains=query)
+    resultados_objetivo = Projeto.objects.filter(objetivo__icontains=query)
+    resultados_resumo = Projeto.objects.filter(resumo__icontains=query)
+
+    resultados = (resultados_titulo | resultados_objetivo | resultados_resumo).distinct()
+
+    # paginator = Paginator(resultados, 3)
+    # numero_da_pagina = request.GET.get('pagina')
+    # resultados_paginados = paginator.get_page(numero_da_pagina)
+
+    projetos_recentes = Projeto.objects.all().order_by("criado_em")[:6]
+
+    context = {
+        'resultados': resultados,
+        'projetos_recentes': projetos_recentes,
+        'query': query,
+    }
+    return render(request, 'explorar.html', context)
