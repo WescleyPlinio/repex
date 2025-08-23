6b6c8f65aa13b0ce18b143c7d5ada4b9d92682dd
Wescley <wescleygaldino06@gmail.com>
2025-08-23 20:50:32 -0300

    Finalizando paginação com Ajax, User Story #10

--- repex/urls.py
+++ repex/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('explorar/', views.explorar, name='explorar'),
+    path("ajax_projetos/", views.ajax_projetos, name="ajax_projetos"),
+    path("ajax_noticias/", views.ajax_noticias, name="ajax_noticias"),
     path('projeto/<int:pk>/', views.ProjetoDetailView.as_view(), name='projeto_detail'),
     path('noticia/<int:pk>/', views.NoticiaDetailView.as_view(), name='noticia_detail'),
 ]
--- repex/views.py
+++ repex/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from repex.models import Projeto, Noticia
 from django.views.generic import DetailView
 from django.core.paginator import Paginator
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 
 def index(request):
     projetos = Projeto.objects.all().order_by("criado_em")[:9]
@@ -44,6 +46,46 @@ def explorar(request):
         'query': query,
     }
     return render(request, 'explorar.html', context)
+
+def ajax_projetos(request):
+    query = request.GET.get('q', '')
+
+    resultado_projeto_titulo = Projeto.objects.filter(titulo__icontains=query)
+    resultado_projeto_objetivo = Projeto.objects.filter(objetivo__icontains=query)
+    resultado_projeto_resumo = Projeto.objects.filter(resumo__icontains=query)
+    resultados_projetos = (resultado_projeto_titulo | resultado_projeto_objetivo | resultado_projeto_resumo).distinct()
+
+    paginator = Paginator(resultados_projetos, 3)
+    page_number = request.GET.get('page_projetos')
+    page_obj = paginator.get_page(page_number)
+
+    html = render_to_string(
+        "partials/_ajax_projetos.html",
+        {"resultados_projetos": page_obj, "query": query},
+        request=request
+    )
+
+    return JsonResponse({"html": html})
+
+def ajax_noticias(request):
+    query = request.GET.get('q', '')
+
+    resultado_noticia_titulo = Noticia.objects.filter(titulo__icontains=query)
+    resultado_noticia_descricao = Noticia.objects.filter(descricao__icontains=query)
+    resultado_noticia_conteudo = Noticia.objects.filter(conteudo__icontains=query)
+    resultados_noticias = (resultado_noticia_titulo | resultado_noticia_descricao | resultado_noticia_conteudo).distinct()
+
+    paginator = Paginator(resultados_noticias, 3)
+    page_number = request.GET.get('page_noticias')
+    page_obj = paginator.get_page(page_number)
+
+    html = render_to_string(
+        "partials/_ajax_noticias.html",
+        {"resultados_noticias": page_obj, "query": query},
+        request=request
+    )
+
+    return JsonResponse({"html": html})
 
 class ProjetoDetailView(DetailView):
     model = Projeto
