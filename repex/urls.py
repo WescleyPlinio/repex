773e2bb869d28fa6526b0cdf748b12fd6f0352b5
Wescley <wescleygaldino06@gmail.com>
2025-09-14 11:20:13 -0300

    Criando model instituição e adicionando ao footer dinamicamente.

--- repex/urls.py
+++ repex/urls.py
@@ -13,12 +13,15 @@ urlpatterns = [
     path('projeto/novo/', views.ProjetoCreateView.as_view(), name='projeto_create'),
     path('noticia/novo/', views.NoticiaCreateView.as_view(), name='noticia_create'),
     path('area_conhecimento/nova/', views.AreaConhecimentoCreateView.as_view(), name='area_conhecimento_create'),
+    path('instituicao/nova/', views.InstituicaoCreateView.as_view(), name='instituicao_create'),
     path('noticia/<int:pk>/editar/', views.NoticiaUpdateView.as_view(), name='noticia_update'),
     path('projeto/<int:pk>/editar/', views.ProjetoUpdateView.as_view(), name='projeto_update'),
     path('area_conhecimento/<int:pk>/editar/', views.AreaConhecimentoUpdateView.as_view(), name='area_conhecimento_update'),
+    path('instituicao/<int:pk>/editar/', views.InstituicaoUpdateView.as_view(), name='instituicao_update'),
     path('projeto/<int:pk>/excluir/', views.ProjetoDeleteView.as_view(), name='projeto_delete'),
     path('noticia/<int:pk>/excluir/', views.NoticiaDeleteView.as_view(), name='noticia_delete'),
     path('area_conhecimento/<int:pk>/excluir/', views.AreaConhecimentoDeleteView.as_view(), name='area_conhecimento_delete'),
+    path('instituicao/<int:pk>/excluir/', views.InstituicaoDeleteView.as_view(), name='instituicao_delete'),
 
 ]
 
